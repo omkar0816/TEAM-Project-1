6066cf191158b54b6a6ab6d7801c3801e5a8621e
omkar0816 <omkarrajumankar@gmail.com>
2026-08-22 13:11:14 +0000

    locationn routes and api rotes for location in index.js , Routes->api location routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const attendanceRoutes = require('./attendanceRoutes');
 const sessionRoutes = require('./sessionRoutes');
 const passwordRoutes = require('./passwordRoutes');
 const bootstrapRoutes = require('./bootstrapRoutes');
+const locationRoutes = require('./locationRoutes');
 
 const router = express.Router();
 router.use(bootstrapRoutes);
@@ -11,5 +12,5 @@ router.use(authRoutes);
 router.use(attendanceRoutes);
 router.use(sessionRoutes);
 router.use(passwordRoutes);
-
+router.use('/api', locationRoutes);
 module.exports = router;
